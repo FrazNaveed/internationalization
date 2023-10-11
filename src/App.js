29a6dbb15b927862c564cbbcd014820319f91cb4
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2023-10-11 11:00:30 +0500

    internationalization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+import { useState } from 'react';
+
+const lngs = {
+    en: { nativeName: 'English' },
+    de: { nativeName: 'Deutsch' }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { t, i18n } = useTranslation();
+    const [count, setCounter] = useState(0);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div>
+                    {Object.keys(lngs).map((lng) => (
+                        <button
+                            key={lng}
+                            style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }}
+                            type="submit"
+                            onClick={() => {
+                                i18n.changeLanguage(lng);
+                                setCounter(count + 1);
+                            }}
+                        >
+                            {lngs[lng].nativeName}
+                        </button>
+                    ))}
+                </div>
+                <p>
+                    <i>{t('counter', { count })}</i>
+                </p>
+                <p>
+                    <Trans i18nKey="description.part1">
+                        Edit <code>src/App.js</code> and save to reload.
+                    </Trans>
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    {t('description.part2')}
+                </a>
+            </header>
+            <div className="Footer">
+                <div>{t('footer.date', { date: new Date() })}</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
